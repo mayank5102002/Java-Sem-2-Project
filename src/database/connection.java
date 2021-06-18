110d4468dbcd0b02212fbecfa201dba1f6227ad4
mayank5102002 <77108996+mayank5102002@users.noreply.github.com>
2021-06-18 22:06:17 +0530

    Add files via upload

---
+++ src/database/connection.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+public class connection {
+
+	Connection con = null;
+	java.sql.PreparedStatement pst;
+	public static Connection dbConnect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
+			return conn;
+		}catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+}
